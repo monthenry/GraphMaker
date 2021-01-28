e94df89a74fb0bb2d16c364def535d9cb7fa5912
Henry MONT <32devstudio@protonmail.com>
2021-01-28 11:46:33 +0100

    toolbar improvement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var currentNodeName=97;
-var currentTool="cursor";
+var currentTool="square";
 var xPos=0;
 var yPos=0;
 
@@ -7,6 +7,7 @@ var yPos=0;
 
 function setTool(toolName) {
   currentTool=toolName;
+  selectButton("button_"+toolName);
 }
 
 onmousemove = function(e){
@@ -14,18 +15,34 @@ onmousemove = function(e){
   yPos=e.clientY;
 }
 
-// document.getElementById("canvas").onmousedown = function () {
-//     console.log("User moused down");
-//     return true; // Not needed, as long as you don't return false
-// };
+function canvasClick(){
+  if (currentTool=="link") {
+    console.log("Link creation");
+  }else if (currentTool=="cursor") {
+    console.log("Mouse pointer interaction");
+  }else {
+    addElement(currentTool);
+  }
+}
 
-function addElement(){
-  var newNode = document.createElement('div');
-  newNode.className = currentTool;
-  newNode.innerHTML = String.fromCharCode(currentNodeName);
-  newNode.style.position = "absolute";
-  newNode.style.left = xPos+"px";
-  newNode.style.top = yPos+"px";
-  document.getElementById('canvas').appendChild(newNode);
-  currentNodeName+=1;
+function selectButton(buttonId){
+  document.getElementById('button_circle').style.border="1px solid #FFFFFF";
+  document.getElementById('button_square').style.border="1px solid #FFFFFF";
+  document.getElementById('button_diamond').style.border="1px solid #FFFFFF";
+  document.getElementById('button_link').style.border="1px solid #FFFFFF";
+  document.getElementById('button_cursor').style.border="1px solid #FFFFFF";
+  document.getElementById(buttonId).style.border="2px solid #FFFFFF";
+}
+
+function addElement(className){
+    xPos=xPos-20;
+    yPos=yPos-20;
+    var newNode = document.createElement('div');
+    newNode.className = className;
+    newNode.innerHTML = String.fromCharCode(currentNodeName);
+    newNode.style.position = "absolute";
+    newNode.style.left = xPos+"px";
+    newNode.style.top = yPos+"px";
+    document.getElementById('canvas').appendChild(newNode);
+    currentNodeName+=1;
 }
