c8b25725282af24191622b65bde7f82a981e96a3
Henry MONT <32devstudio@protonmail.com>
2021-01-28 21:37:28 +0100

    arrow prototype

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ var xPosNode=0;
 var yPosNode=0;
 var editingMod=false;
 
+var x1Line=0;
+var y1Line=0;
+var x2Line=0;
+var y2Line=0;
+
 function setTool(toolName) {
   // check that the inputBox is not open before changing tool
   if (!editingMod) {
@@ -18,11 +23,26 @@ function setTool(toolName) {
 onmousemove = function(e){
   xPos=e.clientX;
   yPos=e.clientY;
+
+  // griddish layout
+  // xPos=(xPos/25|0)*25;
+  // yPos=(yPos/25|0)*25;
 }
 
 // detect mouseclick and launch the appropriate function depending on tool used
 function canvasClick(){
   if (currentTool=="link") {
+    x1Line=xPos;
+    y1Line=yPos;
+
+    // continuous stroke mod
+    // if (Math.abs(x2Line-x1Line)<=50) {
+    //   x1Line=x2Line;
+    // }
+    // if (Math.abs(y2Line-y1Line)<=50) {
+    //   y1Line=y2Line;
+    // }
+
     console.log("Link creation");
   }else if (currentTool=="cursor") {
     console.log("Mouse pointer mod");
@@ -40,6 +60,7 @@ function selectButton(buttonId){
   document.getElementById('button_diamond').style.border="1px solid #FFFFFF";
   document.getElementById('button_link').style.border="1px solid #FFFFFF";
   document.getElementById('button_cursor').style.border="1px solid #FFFFFF";
+  document.getElementById('button_eraser').style.border="1px solid #FFFFFF";
   document.getElementById(buttonId).style.border="2px solid #FFFFFF";
 }
 
@@ -47,10 +68,6 @@ function selectButton(buttonId){
 function addElement(className){
     xPosNode=xPos-20;
     yPosNode=yPos-20;
-
-    // griddish layout
-    // xPos=(xPos/25|0)*25;
-    // yPos=(yPos/25|0)*25;
 
     // shape node
     var newNode = document.createElement('div');
@@ -64,6 +81,7 @@ function addElement(className){
 
     // node containing the text
     var innerNode = document.createElement('div');
+    innerNode.className = 'unselectable';
     innerNode.innerHTML = String.fromCharCode(currentNodeName);
     document.getElementById(currentNodeName).appendChild(innerNode);
 
@@ -96,8 +114,47 @@ function getCaption(){
   editingMod=false;
 }
 
+// erasing function (only if eraser activated)
 function remove() {
   if (currentTool=='eraser') {
     this.remove();
+  }
+}
+
+// drawing line between 2 points
+function drawLine(x1, y1, x2, y2){
+  var heightSvg=Math.abs(y2-y1)+3;
+  var widthSvg=Math.abs(x2-x1)+3;
+
+  // create the svg element
+  var svgArrow = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  // set width and height
+  svgArrow.setAttribute("width", widthSvg.toString());
+  svgArrow.setAttribute("height", heightSvg.toString());
+  svgArrow.style.position = "absolute";
+  svgArrow.style.left = (Math.min(x1, x2)-2)+"px";
+  svgArrow.style.top = (Math.min(y1, y2)-2)+"px";
+
+  // create a line
+  var svgLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  svgLine.setAttribute("x1", (x1-Math.min(x1, x2)).toString());
+  svgLine.setAttribute("x2", (x2-Math.min(x1, x2)).toString());
+  svgLine.setAttribute("y1", (y1-Math.min(y1, y2)).toString());
+  svgLine.setAttribute("y2", (y2-Math.min(y1, y2)).toString());
+  svgLine.setAttribute("class", "arrow");
+
+  // attach it to the container
+  svgArrow.appendChild(svgLine);
+
+  // add arrow in canvas
+  document.getElementById("canvas").appendChild(svgArrow);
+}
+
+function lastPos(){
+  if (currentTool=="link") {
+    x2Line=xPos;
+    y2Line=yPos;
+    drawLine(x1Line, y1Line, x2Line, y2Line);
   }
 }
