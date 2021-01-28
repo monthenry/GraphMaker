26d9be9254e46d6f0ae5ad9c6c0edfa01298ddc5
Henry MONT <32devstudio@protonmail.com>
2021-01-28 15:51:26 +0100

    added caption mechanism for elements

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 var currentNodeName=97;
-var currentTool="square";
+var currentTool="cursor";
 var xPos=0;
 var yPos=0;
-
-// String.fromCharCode(currentNodeName);
+var xPosNode=0;
+var yPosNode=0;
+var editingMod=false;
 
 function setTool(toolName) {
-  currentTool=toolName;
-  selectButton("button_"+toolName);
+  // check that the inputBox is not open before changing tool
+  if (!editingMod) {
+    currentTool=toolName;
+    selectButton("button_"+toolName);
+  }
 }
 
+// get current mouse pos.
 onmousemove = function(e){
   xPos=e.clientX;
   yPos=e.clientY;
 }
 
+// detect mouseclick and launch the appropriate function depending on tool used
 function canvasClick(){
   if (currentTool=="link") {
     console.log("Link creation");
@@ -25,6 +31,7 @@ function canvasClick(){
   }
 }
 
+// handle the outline on the currently used button
 function selectButton(buttonId){
   document.getElementById('button_circle').style.border="1px solid #FFFFFF";
   document.getElementById('button_square').style.border="1px solid #FFFFFF";
@@ -34,17 +41,22 @@ function selectButton(buttonId){
   document.getElementById(buttonId).style.border="2px solid #FFFFFF";
 }
 
+// create a new element (circle, square or diamond)
 function addElement(className){
-    xPos=xPos-20;
-    yPos=yPos-20;
+    xPosNode=xPos-20;
+    yPosNode=yPos-20;
+
+    // griddish layout
+    // xPos=(xPos/25|0)*25;
+    // yPos=(yPos/25|0)*25;
 
     // shape node
     var newNode = document.createElement('div');
     newNode.className = className;
     newNode.id = currentNodeName;
     newNode.style.position = "absolute";
-    newNode.style.left = xPos+"px";
-    newNode.style.top = yPos+"px";
+    newNode.style.left = xPosNode+"px";
+    newNode.style.top = yPosNode+"px";
     document.getElementById('canvas').appendChild(newNode);
 
     // node containing the text
@@ -52,6 +64,30 @@ function addElement(className){
     innerNode.innerHTML = String.fromCharCode(currentNodeName);
     document.getElementById(currentNodeName).appendChild(innerNode);
 
-    // node name incrementation
-    currentNodeName+=1;
+    // opening caption card
+    document.getElementById('inputBox').style.display='flex';
+    setTool('cursor');
+    editingMod=true;
+}
+
+// create a caption for the recently added element
+function getCaption(){
+  var captionValue=document.getElementById('inputField').value;
+
+  // caption node
+  var captionNode = document.createElement('div');
+  captionNode.className = 'nodeCaption';
+  captionNode.id = currentNodeName+"_caption";
+  captionNode.style.position = "absolute";
+  captionNode.style.left = xPosNode+"px";
+  captionNode.style.top = yPosNode+"px";
+  captionNode.innerHTML = captionValue;
+  document.getElementById('canvas').appendChild(captionNode);
+
+  // node name incrementation
+  currentNodeName+=1;
+
+  document.getElementById('inputBox').style.display='none';
+  document.getElementById('inputField').value='';
+  editingMod=false;
 }
